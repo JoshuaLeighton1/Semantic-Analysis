e7bab7a0b6d7d4572f5b590506c09dc44e8140ef
JoshuaLeighton1 <joshuaroelf@gmail.com>
2025-05-24 12:59:51 +0200

    Import necessary libraries

---
+++ sentiment analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import re
+import nltk
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from flask import Flask, request, jsonify
+import pickle
+
+
