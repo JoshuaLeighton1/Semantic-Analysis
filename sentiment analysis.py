927d32d9df3d95dbdd5d0c6a18f3e78195ddc4ae
JoshuaLeighton1 <joshuaroelf@gmail.com>
2025-05-24 13:05:16 +0200

    Load dataset and initial set up

--- sentiment analysis.py
+++ sentiment analysis.py
@@ -11,3 +11,20 @@ from flask import Flask, request, jsonify
 import pickle
 
 
+
+# Download NLTK stopwords (run once)
+nltk.download('stopwords')
+
+#Load the Sentiment140 dataset
+
+df = pd.read_csv('training.1600000.processed.noemoticon.csv', encoding="latin-1", header=None)
+#Assign column names
+df.columns = ['target', 'id', 'date', 'flag', 'user', 'text']
+#keep only sentiment label and tweet text
+df = df[['target', 'text']] 
+#Convert 0 to negative(0) and  4 to positive (1)
+df['target'] = df['target'].map({0:0, 4:1})
+#use a subset of 10 000 samples to ensure efficiency on a standard laptop
+
+df = df.sample(n=10000, random_state=42)
+
